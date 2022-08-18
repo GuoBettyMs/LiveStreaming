58089e17a64b3d1c93e2dd19ad7da2526201c72a
gbt <m15914349592@163.com>
2022-08-18 18:07:15 +0800

    Initial Commit

---
+++ LiveStreaming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveStreaming
+//
+//  Created by gbt on 2022/8/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
